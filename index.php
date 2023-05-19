95f047325d872fd649c7ba088cae99a1e7c8dcf9
SoufianBatta <BTTSFN00H21E573Y@studium.unict.it>
2023-05-19 20:15:28 +0200

    Start

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <script src="script.js"></script>
+    <title>Homepage</title>
+</head>
+<body>
+    <?php 
+    $variavil = 4;
+    $batrest = 7;
+    function somma($a,$b){
+        return $a + $b;
+    } 
+    echo '<h1>' . somma($variavil,$batrest) . '</h1> <h2> la data di oggi e\' '. date("d-m-Y").'</h2>' ; 
+    ?> 
+</body>
+</html>
